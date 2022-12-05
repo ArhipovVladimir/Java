444107a8bb241c0eb2aa80aa7958b559cf4953b4
ArhipovVS <Arhipof05@yandex.ru>
2022-12-05 22:09:24 +0700

    MergSort"

--- HomeWork/Number3/sortMerg.java
+++ HomeWork/Number3/sortMerg.java
@@ -1,25 +1,61 @@
 // 1.Реализовать алгоритм сортировки слиянием
-import java.util.Random;
+
 public class sortMerg {
 
-    static void print(int[] a) {
-        for (int i : a) {
+    public static void main(String[] args) {
+        int[] a = { 5, 1, 6, 2, 3, 4 };
+        
+        System.out.println("не сортирован");
+        print (a);
+        mergeSort(a, a.length);
+        System.out.println("сортирован");
+        print (a);
+       
+    }
+    static void print(int[] arr) {
+        for (int i : arr) {
             System.out.printf("%d, ", i);
 
         }
+        System.out.println();
+    }
+   
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2)
+            return;
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
     }
 
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
 
+        int i = 0, j = 0, k = 0;
 
-    public static void main(String[] args) {
-        int[] arr = new int[10];
-        
-        for (int i = 0; i < arr.length; i++) {
-                arr[i] = Math.random() * (n + 1);
+        while (i < left && j < right) {
 
-            }
-        
-        print(arr);
-        
+            if (l[i] <= r[j])
+                a[k++] = l[i++];
+            else
+                a[k++] = r[j++];
+
+        }
+
+        while (i < left)
+            a[k++] = l[i++];
+
+        while (j < right)
+            a[k++] = r[j++];
     }
 }
