a91a460ecb63015719af59d655d3c9875b04fb24
ArhipovVS <Arhipof05@yandex.ru>
2022-12-08 23:23:02 +0700

    HM4

--- HomeWork/Number4/Qvesr.java
+++ HomeWork/Number4/Qvesr.java
@@ -19,5 +19,37 @@ public class Qvesr {
             }
         System.out.println("Исходный");  
         System.out.println(ll); 
+        enqueue(ll, 9);
+        System.out.println("добавил элемент - 9 ");
+        System.out.println("список с элементом");
+        System.out.println(ll);
+        System.out.println("первый элемент в очереди");
+        System.out.println(dequeue(ll));
+        System.out.println("список без элемента");
+        System.out.println(ll);
+        System.out.println(first(ll));
+        System.out.println("элемент не удален");
+        System.out.println(ll);
     }
-}
+
+    public static void enqueue(LinkedList<Integer> list, int num) {
+        list.addLast(num);
+    }
+
+    public static int dequeue(LinkedList<Integer> list) {
+        int num = 0;
+        num = list.get(0);
+        list.remove(0);
+        return num;
+    }
+
+    public static int first(LinkedList<Integer> list) {
+        int num = 0;
+        num = list.get(0);
+        return num;
+    }
+    
+    
+    
+    }
+
